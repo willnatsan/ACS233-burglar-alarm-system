d6532caf9075e92c7019988108511f882f5ddc53
willnatsan <sannatwill@gmail.com>
2024-04-28 14:21:34 +0100

    Added adiditional attributes and methods for Controller module

--- src/Controller.h
+++ src/Controller.h
@@ -11,20 +11,30 @@
 
 class Controller {
     private:
+        // System Timeouts 
         float UNLOCK_TIMEOUT = 10000; // Random value for now
         float ALARM_TIMEOUT = 10000; // Random value for now
         float PIN_ENTRY_TIMEOUT = 10000; // Random value for now
         float MOTION_SENSOR_TIMEOUT = 10000; // Random value for now
 
-        float last_armed_at;
-
+        // Sensors
         MagneticSensor *door;
         MagneticSensor *window;
         PIRSensor *pir; 
         ButtonSensor *button;
 
+        // Actuators
         Buzzer *buzzer;
         Solenoid *lock;
+        LED *diarmed_led;
+        LED *home_led;
+        LED *away_led;
+        
+        // Time at which the system was last armed
+        float last_armed_at;
+
+        // Tracking if the system recognises an authorised user or not
+        bool authorisation_status;
 
         void magnetic_sensor_isr();
         void pir_sensor_isr();
@@ -32,16 +42,17 @@ class Controller {
 
     public:
         explicit Controller(int magnetic_sensor_pin, int pir_sensor_pin, int button_pin,
-                        int disarmed_mode_led, int home_mode_led, int away_mode_led,
+                        int disarmed_led_pin, int home_led_pin, int away_led_pin,
                         int solenoid_pin,int buzzer_pin);
         void setup();
-        void verify_user();
+        void authorise_user();
         void change_mode(SYSTEM_MODE mode);
         void alarm_on();
         void alarm_off();
         void handle_unauthorised_entry(UNAUTHORISED_ENTRY_TYPE type);
         void handle_authorised_entry();
         void check_status();
+        void change_settings();
 
 };
 
