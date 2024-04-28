d2e856da96fd3273aeb524eb650ae8ad9fbadb2a
willnatsan <sannatwill@gmail.com>
2024-04-29 00:16:59 +0100

    Finished Output Testing (Success!)

--- examples/OutputTest/OutputTest.ino
+++ examples/OutputTest/OutputTest.ino
@@ -1,7 +1,47 @@
+int red_led = 4;
+int yellow_led = 3;
+int green_led = 2;
+int solenoid = 13;
+int buzzer = 12;
+
 void setup() {
-  // put your setup code here, to run once:
+  // put your setup code here, to run once
+  
+  pinMode(red_led, OUTPUT);
+  pinMode(yellow_led, OUTPUT);
+  pinMode(green_led, OUTPUT);
+  pinMode(solenoid, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+
+  tone(buzzer, 500);
+  delay(1000);
+  noTone(buzzer);
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(red_led, HIGH);
+  digitalWrite(yellow_led, LOW);
+  digitalWrite(green_led, LOW);
+  delay(1000);
+
+  digitalWrite(red_led, LOW);
+  digitalWrite(yellow_led, HIGH);
+  digitalWrite(green_led, LOW);
+  delay(1000);
+
+  digitalWrite(red_led, LOW);
+  digitalWrite(yellow_led, LOW);
+  digitalWrite(green_led, HIGH);
+  delay(1000);
+
+  digitalWrite(solenoid, HIGH);
+  delay(1000);
+  digitalWrite(solenoid, LOW);
+  delay(1000);
+  digitalWrite(solenoid, HIGH);
+  delay(1000);
+  digitalWrite(solenoid, LOW);
+  delay(1000);
+  
 }
