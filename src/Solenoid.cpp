16f06e9678fdecfb22a59c4961af4fde2072d8b6
willnatsan <sannatwill@gmail.com>
2024-04-29 00:10:25 +0100

    Swicthed around Solenoid lock() and unlock() to reflect hardware behaviour

--- src/Solenoid.cpp
+++ src/Solenoid.cpp
@@ -2,17 +2,17 @@
 
 Solenoid::Solenoid(uint8_t pin) {
   this->solenoid_pin = pin;
-  this->solenoid_state = SOLENOID_STATE::UNLOCKED;
+  this->solenoid_state = SOLENOID_STATE::LOCKED;
   this->last_unlocked_at = -1;
   pinMode(this->solenoid_pin, OUTPUT);
 }
 
 void Solenoid::lock() {
-  digitalWrite(this->solenoid_pin, HIGH);
+  digitalWrite(this->solenoid_pin, LOW);
   this->solenoid_state = SOLENOID_STATE::LOCKED;
 }
 
 void Solenoid::unlock() {
-  digitalWrite(this->solenoid_pin, LOW);
+  digitalWrite(this->solenoid_pin, HIGH);
   this->solenoid_state = SOLENOID_STATE::UNLOCKED;
 }
