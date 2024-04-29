db610e8f99d9ee93de75d26ee5f3f5bb5ec36d7d
willnatsan <sannatwill@gmail.com>
2024-04-29 05:01:53 +0100

    Assigned pin configs in burglarAlarm.ino according to circuit layout

--- examples/burglarAlarm/burglarAlarm.ino
+++ examples/burglarAlarm/burglarAlarm.ino
@@ -1,3 +1,12 @@
+#define SOLENOID_PIN 13
+#define BUZZER_PIN 12
+#define AWAY_LED_PIN 4
+#define DISARMED_LED_PIN 3
+#define HOME_LED_PIN 2
+#define MAGNETIC_SENSOR_PIN 19
+#define PIR_SENSOR_PIN 20
+#define BUTTON_PIN 21
+
 void setup() {
   // put your setup code here, to run once:
 
