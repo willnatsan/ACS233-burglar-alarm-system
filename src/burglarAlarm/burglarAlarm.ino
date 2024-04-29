4c6ea3d2558fdd8cc27687f2f9a650dbb7a5d5bb
willnatsan <sannatwill@gmail.com>
2024-04-29 18:23:35 +0100

    Wrote basic structure for main burglarAlarm.ino file
    Depending on how the rest of the codebase pans out, the main file to be
    uploaded could just look like this. But we'll see

--- src/burglarAlarm/burglarAlarm.ino
+++ src/burglarAlarm/burglarAlarm.ino
@@ -1,18 +1,33 @@
 #include "Controller.h"
 
-#define SOLENOID_PIN 13
-#define BUZZER_PIN 12
-#define AWAY_LED_PIN 4
-#define DISARMED_LED_PIN 3
-#define HOME_LED_PIN 2
-#define MAGNETIC_SENSOR_PIN 19
-#define PIR_SENSOR_PIN 20
-#define BUTTON_PIN 21
+uint8_t SOLENOID_PIN = 13;
+uint8_t BUZZER_PIN = 12;
+uint8_t AWAY_LED_PIN = 4;
+uint8_t DISARMED_LED_PIN = 3;
+uint8_t HOME_LED_PIN = 2;
+uint8_t WINDOW_MAG_PIN = 18;
+uint8_t DOOR_MAG_PIN = 19;
+uint8_t PIR_SENSOR_PIN = 20;
+uint8_t BUTTON_PIN = 21;
+
+Controller controller(DOOR_MAG_PIN, WINDOW_MAG_PIN, PIR_SENSOR_PIN, BUTTON_PIN,
+                      DISARMED_LED_PIN, HOME_LED_PIN, AWAY_LED_PIN,
+                      SOLENOID_PIN, BUTTON_PIN);
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  controller.setup();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+  switch (controller.current_mode) {
+  case SYSTEM_MODE::DISARMED:
+    controller.disarmed_mode();
+    break;
+  case SYSTEM_MODE::HOME:
+    controller.home_mode();
+    break;
+  case SYSTEM_MODE::AWAY:
+    controller.away_mode();
+    break;
+  }
